1c3aaf4f4469f9bc537471dc8c9a53b486149214
JulieRaph <julie.tolve@gmail.com>
2025-05-26 11:40:10 +0200

    Implement renderTable function to display students in a table format

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -17,6 +17,36 @@ const student2: Student = {
     lastName: "Doe",
     age: 19,
     location: "Seattle"
-}
+};
 
 const studentsList: Student[] = [student1, student2];
+
+// Function to create and display a table
+function renderTable(): void {
+    // Create table and body elements
+    const table = document.createElement('table');
+    const tbody = document.createElement('tbody');
+
+    // Throw each element student in the studentsList into a row
+    studentsList.forEach(student => {
+        const row = document.createElement('tr');
+
+        // Create a cell for the firstName
+        const firstNameColumn = document.createElement('td');
+        firstNameColumn.textContent = student.firstName;
+        row.appendChild(firstNameColumn);
+
+        // Create a cell for the location
+        const locationColumn = document.createElement('td');
+        locationColumn.textContent = student.location;
+        row.appendChild(locationColumn);
+
+        tbody.appendChild(row);
+    });
+
+    // Append the body to the table
+    table.appendChild(tbody);
+    document.body.appendChild(table);
+}
+// To execute the function and render the table
+renderTable();
